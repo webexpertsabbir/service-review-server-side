9c0704edfde71c8cd56f7f3b3b1346700f8f0209
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-11-10 20:14:21 +0600

    stacther

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sabbir.0dgpj5g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -37,7 +35,6 @@ async function run(){
 }
 
 run();
-
 
 app.get('/', (req, res) => {
     res.send('Photography car server in running');
