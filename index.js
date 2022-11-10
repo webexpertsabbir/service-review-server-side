338295dc4e5f4063a9f631e5fe6bd6647151986c
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-11-10 12:17:47 +0600

    service data

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sabbir.0dgpj5g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+    const serviceCollection = client.db('photography').collection('services')
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+        
+    }
+    run().catch(err => console.error(err));
+}
+
+run();
+
+
 
 app.get('/', (req, res) => {
     res.send('Photography car server in running');
